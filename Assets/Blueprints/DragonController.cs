14965dfeb9f9ade15b1cb8efb0b2ca0517586b07
Agrim Singh <59444327+th-efool@users.noreply.github.com>
2025-03-16 06:28:14 +0530

    FlightImproved
    Bug on disabling gravity, when i tried to give input it wasn't taking it, i then instead of disabling gravity, added an aantigravity force that too wasn't working - realized that it was down direction i was adding force.
    as for why movement so weird in flight can'tmove it in direction of joystick - reason no drag in air, once object reaces max velocity in one direction, it stays that direction foreevver, and doesn't register new inputs.
    added rigidbody.damp

--- Assets/Blueprints/DragonController.cs
+++ Assets/Blueprints/DragonController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,14 +11,16 @@ public class DragonController : MonoBehaviour
     int DirectionHash;
     int TakeOffHash;
     bool InAir;
+    Vector3 AntiGravitationalForce;
     [SerializeField] float MAX_SPEED =10f;
     [SerializeField] float FLIGHT_SPEED_MULTIPLIYER = 8f;
     [SerializeField] float GROUND_ACCELERATION = 15f;
-    [SerializeField] float FLIGHT_ACCELERATION_MULTIPLIYER = 5f;
+    [SerializeField] float FLIGHT_ACCELERATION_MULTIPLIYER = 5f;    
+    [SerializeField] float ANTIGRAVITY_MULTIPLIER = 1.02f;
 
 
 
-    public float debugVelocity;
+    public Vector3 PureHorizontalVelocity;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -25,22 +28,46 @@ public class DragonController : MonoBehaviour
         DirectionHash = Animator.StringToHash("Direction");
         TakeOffHash = Animator.StringToHash("TakeOff");
         IADragon = new IADragon();
+        AntiGravitationalForce = new Vector3(0, (float)(-Physics.gravity.y* ANTIGRAVITY_MULTIPLIER),0);
 
 
     }
     private void FixedUpdate()
     {
-        debugVelocity = rb.linearVelocity.magnitude;
-        animator.SetFloat(DirectionHash, (rb.linearVelocity.magnitude) / MAX_SPEED); 
+        PureHorizontalVelocity = rb.linearVelocity;
+        PureHorizontalVelocity.y = 0;
         Vector3 MovementDirection = new(joystick.Horizontal, 0, joystick.Vertical);
-        if (rb.linearVelocity.magnitude < MAX_SPEED) {rb.AddForce(MovementDirection * GROUND_ACCELERATION, ForceMode.Acceleration); }
+        if (PureHorizontalVelocity.magnitude < MAX_SPEED) {rb.AddForce(MovementDirection * GROUND_ACCELERATION, ForceMode.Acceleration); }
         if (joystick.Vertical >0.2 || joystick.Horizontal > 0.2 || (joystick.Vertical < -0.2) || joystick.Horizontal < -0.2) { 
         transform.rotation  = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(MovementDirection), 0.05f );}
+        if (InAir) { StayAfloat(); }
+        animator.SetFloat(DirectionHash, (PureHorizontalVelocity.magnitude) / MAX_SPEED);
+
     }
 
     private void OnEnable()
     {
         IADragon.Enable();
-        IADragon.Locomotion.Fly.started += ctx => { if (InAir) { InAir = false; animator.SetBool(TakeOffHash, false); MAX_SPEED = MAX_SPEED/FLIGHT_SPEED_MULTIPLIYER; GROUND_ACCELERATION = GROUND_ACCELERATION/FLIGHT_ACCELERATION_MULTIPLIYER; } else { InAir = true; animator.SetBool(TakeOffHash, true); MAX_SPEED = MAX_SPEED * FLIGHT_SPEED_MULTIPLIYER; GROUND_ACCELERATION = GROUND_ACCELERATION * FLIGHT_ACCELERATION_MULTIPLIYER; } };
+        IADragon.Locomotion.Fly.started += TakeFlight;
     }
+
+    void TakeFlight(InputAction.CallbackContext callbackContext){
+    if (InAir) {ExitFlight();}
+    else { InAir = true; animator.SetBool(TakeOffHash, true); 
+        MAX_SPEED = MAX_SPEED * FLIGHT_SPEED_MULTIPLIYER; 
+        GROUND_ACCELERATION = GROUND_ACCELERATION * FLIGHT_ACCELERATION_MULTIPLIYER;
+            rb.linearDamping = 1.0f;
+        }
+    }
+
+    void ExitFlight()
+    {
+        InAir = false; animator.SetBool(TakeOffHash, false);
+        MAX_SPEED = MAX_SPEED / FLIGHT_SPEED_MULTIPLIYER;
+        GROUND_ACCELERATION = GROUND_ACCELERATION / FLIGHT_ACCELERATION_MULTIPLIYER;
+        rb.linearDamping = 0;
+
+    }
+    void StayAfloat() { rb.AddForce(AntiGravitationalForce,ForceMode.Acceleration); }
 } 
+
