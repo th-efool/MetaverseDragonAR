78b146553a905428885f90abacc2dc8809d99edc
Agrim Singh <59444327+th-efool@users.noreply.github.com>
2025-03-18 23:51:34 +0530

    Added Sound Stuff + Loading Scene % set up, difficult to gain reference to text object

--- Assets/Blueprints/Singletons/CustomSceneLoader.cs
+++ Assets/Blueprints/Singletons/CustomSceneLoader.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Threading;
+using TMPro;
 
 public class CustomSceneLoader : Singleton<CustomSceneLoader>
 {
+    int sceneloaded;
+    TMP_Text ProgressText;
     public void LoadScene(LevelList SelectedLevel)
     {
         SceneManager.LoadSceneAsync("LoadingScene",LoadSceneMode.Single);
+
         StartCoroutine(AsyncLoad(SelectedLevel));
     }
 
@@ -19,7 +23,7 @@ public class CustomSceneLoader : Singleton<CustomSceneLoader>
         AsyncLoadedScene.allowSceneActivation = false;
 
         yield return new WaitUntil(() => AsyncLoadedScene.progress >= 0.9f);
-
+        ProgressText = null;
         AsyncLoadedScene.allowSceneActivation = true;
 
         yield return new WaitUntil(() => AsyncLoadedScene.isDone);
